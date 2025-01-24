43ea635d9e04efc2e375e84d9463fb0684e516d3
Sarinco <deleuxantoine@gmail.com>
2025-01-24 21:26:51 +0100

    orders start

---
+++ Event_sourcing_tests/monolith-project/src/back-end/orders/src/routes/orderRoute.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send("TODO")
+});
+
+router.post('/', (req, res) => {
+    res.send("TODO")
+});
+
+router.put('/', (req, res) => {
+    res.send("TODO")
+});
+
+export default router;
