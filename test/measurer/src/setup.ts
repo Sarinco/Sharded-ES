6a4bbc01827a8357ce18916e8f798be159846b17
MxDx <maxime.dxl2002@gmail.com>
2025-05-03 11:52:15 +0200

    better file name output

--- test/measurer/src/setup.ts
+++ test/measurer/src/setup.ts
@@ -7,6 +7,9 @@ after(() => {
     const measurementService = MeasurementService.getInstance();
     const numberOfMeasurements = measurementService.getMeasurements().length;
     console.log("Number of measurements:", numberOfMeasurements);
-    const filePath = "measurements.json";
+    const date = new Date();
+    // Get the date string in YYYY-MM-DD-HH:MM:SS format
+    const date_string = date.toISOString().split("T")[0] + "-" + date.toTimeString().split(" ")[0];
+    const filePath = `../measurements/measurement_${date_string}.json`;
     measurementService.writeToFile(filePath);
 });
